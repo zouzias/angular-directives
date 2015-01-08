e0f1de3ec9b299c00504d4229b5921d13e6e3d46
Anastasios Zouzias <zouzias@gmail.com>
2015-01-08 17:09:58 +0100

    First yeoman commit

---
+++ d3/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name d3App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the d3App
+ */
+angular.module('d3App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ d3/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name d3App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the d3App
+ */
+angular.module('d3App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
