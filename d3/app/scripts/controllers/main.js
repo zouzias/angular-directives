ffd822d3539af3ffb034140b1b1dfcb431614341
Anastasios Zouzias <azo@zurich.ibm.com>
2015-01-13 10:35:27 +0100

    Updated data. Random numbers have 2 decimal digits.

--- d3/app/scripts/controllers/main.js
+++ d3/app/scripts/controllers/main.js
@@ -10,19 +10,22 @@
 angular.module('d3App')
   .controller('MainCtrl', function ($scope) {
 
-    $scope.data = [{"name": "hello wolrd", "score": 10},
-                    {"name": "world", "score": 15},
+    $scope.data = [{'name': 'hello wolrd', 'score': 10},
+                    {'name': 'world', 'score': 15},
       {'name' : 'third item','score' : 3}];
 
+    var myRand = function(){
+      return Number(Math.random().toFixed(2));
+    };
 
-    $scope.scatter =  [ {'text': 'a', 'x': Math.random(), 'y' : Math.random()},
-    {'text': 'b', 'x': Math.random(), 'y' : Math.random()},
-    {'text': 'c', 'x': Math.random(), 'y' : Math.random()},
-    {'text': 'd', 'x': Math.random(), 'y' : Math.random()},
-    {'text': 'e', 'x': Math.random(), 'y' : Math.random()},
-    {'text': 'f', 'x': Math.random(), 'y' : Math.random()},
-    {'text': 'a', 'x': Math.random(), 'y' : Math.random()},
-    {'text': 'b', 'x': Math.random(), 'y' : Math.random()}, ];
+    $scope.scatter =  [ {'text': 'Greece', 'x': myRand(), 'y' : myRand()},
+    {'text': 'Italy', 'x': myRand(), 'y' : myRand()},
+    {'text': 'Greece', 'x': myRand(), 'y' : myRand()},
+    {'text': 'Canada', 'x': myRand(), 'y' : myRand()},
+    {'text': 'Greece', 'x': myRand(), 'y' : myRand()},
+    {'text': 'Canada', 'x': myRand(), 'y' : myRand()},
+    {'text': 'USA', 'x': myRand(), 'y' : myRand()},
+    {'text': 'Croatia', 'x': myRand(), 'y' : myRand()}, ];
 
     $scope.d3OnClick = function (item) {
       console.log('Item is ' + item.name);
