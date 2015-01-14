04466663c018147464684ea94d977436928ee7e9
Anastasios Zouzias <azo@zurich.ibm.com>
2015-01-14 13:07:24 +0100

    Added a reload button. Code refactor

--- d3/app/scripts/controllers/main.js
+++ d3/app/scripts/controllers/main.js
@@ -10,15 +10,21 @@
 angular.module('d3App')
   .controller('MainCtrl', function ($scope) {
 
-    $scope.data = [{'name': 'hello wolrd', 'score': 10},
-                    {'name': 'world', 'score': 15},
-      {'name' : 'third item','score' : 3}];
-
-    var myRand = function(){
-      return Number(Math.random().toFixed(2));
+     var myRand = function(){
+      return Number(10 * Math.random().toFixed(2));
     };
 
-    $scope.scatter =  [ {'text': 'Greece', 'x': myRand(), 'y' : myRand()},
+    var randInteger = function(n){
+      return Math.floor(Math.random() * (n + 1));
+    };
+
+    $scope.reload = function(){
+      
+          $scope.data = [{'name': 'hello wolrd', 'score': randInteger(10)},
+                    {'name': 'world', 'score': randInteger(10)},
+      {'name' : 'third item','score' : randInteger(10)}];
+
+      $scope.scatter =  [ {'text': 'Greece', 'x': myRand(), 'y' : myRand()},
     {'text': 'Italy', 'x': myRand(), 'y' : myRand()},
     {'text': 'Greece', 'x': myRand(), 'y' : myRand()},
     {'text': 'Canada', 'x': myRand(), 'y' : myRand()},
@@ -32,12 +38,17 @@ angular.module('d3App')
 
     for ( var i = 1; i <= 7; i++){
       for ( var j = 1 ; j <= 24; j++){
-        var o = { 'day' : i , 'hour': j, 'value' :  Math.floor(Math.random() * 11)};
+        var o = { 'day' : i , 'hour': j, 'value' :  randInteger(10)};
         $scope.heatmap.push(o);
       }
     }
 
+    };
+   
+
     $scope.d3OnClick = function (item) {
       console.log('Item is ' + item.name);
     };
+
+    $scope.reload();
   });
